92df1e664d0e6cb09d9fc26e2a67fb8fab8864ac
David Akang <akangdavd68@yahoo.com>
2024-04-30 20:29:43 +0100

    Add Cypress and Tailwind configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+import withMT from '@material-tailwind/react/utils/withMT';
+
+module.exports = withMT({
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
