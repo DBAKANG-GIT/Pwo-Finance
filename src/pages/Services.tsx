6460d91497ad628619b84603f71f00a11e9ea906
David Akang <akangdavd68@yahoo.com>
2024-05-18 19:15:26 +0100

    Add OurServices component to Services page

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import OurServices from '../components/Services-section/OurServices';
 
 const Services: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to the Services page!</h1>
+    <div className="min-h-screen bg-[#f3e6de]">
+      <OurServices />
       {/* Add your content here */}
     </div>
   );
